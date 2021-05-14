b2be16bed67aed94e52d9943cabe6cafc8e62b61
Yusuf Ipek <yusufipek@protonmail.com>
2021-05-14 12:31:45 +0300

    created and styled footer

--- src/commons/footer.jsx
+++ src/commons/footer.jsx
@@ -1,3 +1,49 @@
 export default function Footer() {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <ul className="nav">
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Request proposal
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Download homeplanner
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact us
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Submit your property
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Come work with us!
+          </a>
+        </li>
+      </ul>
+      <p className="copyright">
+        &copy; Designed by Jonas Schmedtmann and coded by{" "}
+        <a
+          href="https://yusufipek.me"
+          target="_blank"
+          rel="noreferrer"
+          className="copyright__link"
+        >
+          Yusuf İpek
+        </a>
+      </p>
+    </footer>
+  );
 }
