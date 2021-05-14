e628868b40f6af46fd71658257286119e7f8191c
Yusuf Ipek <yusufipek@protonmail.com>
2021-05-14 12:52:33 +0300

    added sidebar icon

--- src/commons/sidebar.jsx
+++ src/commons/sidebar.jsx
@@ -1,3 +1,9 @@
 export default function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <button className="nav-btn">
+        <i class="fa fa-bars" />
+      </button>
+    </div>
+  );
 }
